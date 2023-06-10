56ae11cb7844dd95e70a9a62920b86914848f23e
lyks <lykamaysaavedra02@gmail.com>
2023-06-10 17:32:43 +1000

    no changes just modified

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,9 +2,6 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your Profile <span class="bg-gray-800 text-white"> {{ Auth::user()->name }}</span>
+        Welcome to your Profile <span class="text-red-800"> {{ Auth::user()->name }}</span>
     </h1>
-</div>
-<div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-    
 </div>
